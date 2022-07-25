7ee9b26be9ac8559148772bd30e65e657b59e3cb
GBearr <muhammedmuratc@gmail.com>
2022-07-25 17:40:31 +0300

    Removed Navigation Bar

--- App.js
+++ App.js
@@ -11,7 +11,7 @@ const navigator = createStackNavigator(
   {
     initialRouteName: 'Home',
     defaultNavigationOptions: {
-      title: 'Education App Demo',
+      headerShown: false,
     },
   },
 );
