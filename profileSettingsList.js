654e3fdf36cc0f72ffc2a9a1dd05cdeb0787a773
GBearr <muhammedmuratc@gmail.com>
2022-08-03 15:00:16 +0300

    ProfilScreen

---
+++ profileSettingsList.js
@@ -0,0 +1,33 @@
+const profileSettingsData = () => {
+  return {
+    profileSettings: [
+      {
+        image:
+          'https://images.pexels.com/photos/12292899/pexels-photo-12292899.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+        text: 'Settings',
+      },
+      {
+        image:
+          'https://images.pexels.com/photos/12292899/pexels-photo-12292899.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+        text: 'Settings',
+      },
+      {
+        image:
+          'https://images.pexels.com/photos/12292899/pexels-photo-12292899.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+        text: 'Settings',
+      },
+      {
+        image:
+          'https://images.pexels.com/photos/12292899/pexels-photo-12292899.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+        text: 'Settings',
+      },
+      {
+        image:
+          'https://images.pexels.com/photos/12292899/pexels-photo-12292899.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+        text: 'Settings',
+      },
+    ],
+  };
+};
+
+export default profileSettingsData;
--- src/screens/ProfileScreen.js
+++ src/screens/ProfileScreen.js
@@ -1,17 +1,20 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {
   Text,
   View,
   StyleSheet,
   Image,
   TouchableOpacity,
-  ScrollView,
+  TextInput,
 } from 'react-native';
 import {FlatList} from 'react-native-gesture-handler';
 import profileList from '../../profileList';
+import profileSettingsList from '../../profileSettingsList';
 
-const ProfileScreen = () => {
+const ProfileScreen = props => {
   const profileData = profileList();
+  const profileSettingsData = profileSettingsList();
+  const [counter, setCounter] = useState('');
 
   return (
     <View style={styles.viewStyle}>
@@ -21,54 +24,41 @@ const ProfileScreen = () => {
           horizontal
           showsHorizontalScrollIndicator={false}
           renderItem={item => (
-            <View>
-              <Image
-                source={{uri: item.item.image}}
-                style={styles.imageStyle}
-              />
-              <Text style={styles.textStyle}>{item.item.userName}</Text>
-            </View>
+            <TouchableOpacity>
+              <View>
+                <Image
+                  source={{uri: item.item.image}}
+                  style={styles.imageStyle}
+                />
+                <Text style={styles.textStyle}>{item.item.userName}</Text>
+              </View>
+            </TouchableOpacity>
           )}
         />
       </View>
-      <View style={styles.allButtonViewStyle}>
-        <View style={styles.buttonViewStyle}>
-          <TouchableOpacity style={styles.buttonStyle}>
-            <Image
-              source={require('../../Icons/settings.png')}
-              style={styles.buttonImageStyle}
-            />
-            <Text style={styles.textStyle}>Ayarlar</Text>
-          </TouchableOpacity>
-          <TouchableOpacity style={styles.buttonStyle}>
-            <Image
-              source={require('../../Icons/settings.png')}
-              style={styles.buttonImageStyle}
-            />
-            <Text style={styles.textStyle}>Tivibu Ayarları</Text>
-          </TouchableOpacity>
-        </View>
-        <View style={styles.buttonViewStyle}>
-          <TouchableOpacity style={styles.buttonStyle}>
-            <Image
-              source={require('../../Icons/settings.png')}
-              style={styles.buttonImageStyle}
-            />
-            <Text style={styles.textStyle}>Tivibu Ayarları</Text>
-          </TouchableOpacity>
-          <TouchableOpacity style={styles.buttonStyle}>
-            <Image
-              source={require('../../Icons/settings.png')}
-              style={styles.buttonImageStyle}
-            />
-            <Text style={styles.textStyle}>Tivibu Ayarları</Text>
-          </TouchableOpacity>
-        </View>
-        <View style={styles.buttonViewStyle}>
-          <TouchableOpacity style={styles.quitButtonStyle}>
-            <Text style={styles.textStyle}>Çıkış Yap</Text>
-          </TouchableOpacity>
-        </View>
+      <View style={styles.settingsButtonViewStyle}>
+        <FlatList
+          data={profileSettingsData.profileSettings}
+          numColumns={2}
+          renderItem={item => (
+            <TouchableOpacity style={styles.buttonStyle}>
+              <View style={{alignItems: 'center'}}>
+                <Image
+                  source={{uri: item.item.image}}
+                  style={styles.buttonImageStyle}
+                />
+                <Text style={styles.textStyle}>{item.item.text}</Text>
+              </View>
+            </TouchableOpacity>
+          )}
+        />
+      </View>
+      <View style={styles.quitViewStyle}>
+        <TouchableOpacity
+          style={styles.quitButtonStyle}
+          onPress={() => props.navigation.navigate('Login')}>
+          <Text style={styles.textStyle}>Çıkış</Text>
+        </TouchableOpacity>
       </View>
     </View>
   );
@@ -81,28 +71,39 @@ const styles = StyleSheet.create({
     backgroundColor: '#09182b',
     flex: 1,
     alignItems: 'center',
-    justifyContent: 'space-around',
+    // Space-arround ekleme
+  },
+  settingsButtonViewStyle: {
+    backgroundColor: '#09182b',
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  quitViewStyle: {
+    width: '80%',
+    height: 50,
+    marginBottom: 20,
+    alignItems: 'center',
   },
   quitButtonStyle: {
     width: '80%',
-    height: 80,
+    height: 50,
     backgroundColor: 'red',
     alignItems: 'center',
     textAlign: 'center',
   },
   buttonImageStyle: {
-    width: 40,
-    height: 40,
-    textAlign: 'center',
-    alignSelf: 'center',
-    justifyContent: 'center',
-    marginTop: 30,
+    width: 80,
+    height: 80,
+    borderRadius: 50,
   },
   buttonStyle: {
     borderWidth: 0.5,
     borderColor: 'gray',
-    width: 150,
-    height: 150,
+    marginLeft: 10,
+    marginBottom: 10,
+    width: 120,
+    height: 120,
     justifyContent: 'space-around',
   },
   allButtonViewStyle: {
